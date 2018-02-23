b2c3f80c9d7d3a7770a348ed8d708a917c6d23f5
Vanheden <smultronstigen12@gmail.com>
2018-02-23 21:06:58 +0100

    Initial commit

---
+++ __resource.lua
@@ -0,0 +1,21 @@
+dependency 'essentialmode'
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/de.lua',
+	'locales/br.lua',
+	'locales/en.lua',
+	'locales/fr.lua',
+	'config.lua',
+	'client/client.lua'
+}
+
+server_scripts {
+	'@es_extended/locale.lua',
+	'locales/de.lua',
+	'locales/br.lua',
+	'locales/en.lua',
+	'locales/fr.lua',
+	'config.lua',
+	'server/server.lua'
+}
---
+++ config.lua
@@ -0,0 +1,30 @@
+Config = {}
+Config.Locale = 'en'
+Config.NumberOfCopsRequired = 1
+
+Banks = {
+	["fleeca"] = {
+		position = { ['x'] = 147.04908752441, ['y'] = -1044.9448242188, ['z'] = 29.36802482605 },
+		reward = math.random(1000000,2000000),
+		nameofbank = "Nordea",
+		lastrobbed = 0
+	},
+	["fleeca2"] = {
+		position = { ['x'] = -2957.6674804688, ['y'] = 481.45776367188, ['z'] = 15.697026252747 },
+		reward = math.random(15000,25000),
+		nameofbank = "Fleeca Bank (Highway)",
+		lastrobbed = 0
+	},
+	["blainecounty"] = {
+		position = { ['x'] = -107.06505584717, ['y'] = 6474.8012695313, ['z'] = 31.62670135498 },
+		reward = math.random(15000,25000),
+		nameofbank = "Blaine County Savings",
+		lastrobbed = 0
+	},
+	["PrincipalBank"] = {
+		position = { ['x'] = 255.001098632813, ['y'] = 225.855895996094, ['z'] = 101.005694274902 },
+		reward = math.random(1500000,3500000),
+		nameofbank = "Swedbank",
+		lastrobbed = 0
+	}
+}
